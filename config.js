b94ec4ff8afac193edb4c63510cf29d414e90f63
egwiazda <egwiazda@gmail.com>
2017-06-16 21:28:31 -0300

    Primera Version

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'app-edu',
+		wp_ws_url : 'http://fancv.url.ph/wp-appkit-api/app-edu',
+		wp_url : 'http://fancv.url.ph',
+		theme : 'q-android',
+		version : '1',
+		app_title : 'App Edu',
+		app_platform : 'android',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'iTo0vQ8vK4qugosx4Bk6VfVv6Ng2CUhH08AfQHrfEgvy9e51ODxp6lRL5PocI7LE',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
